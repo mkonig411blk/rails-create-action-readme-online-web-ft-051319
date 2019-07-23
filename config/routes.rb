409a0bee9cd6684a6535f611996bffccc1dca08f
Margot Konig <margot.konig@blackrock.com>
2019-07-23 01:03:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index, :new, :create]
-  get '/post/:id', to: 'posts#show', as: 'post'
+  resources :posts, only: [:index, :new, :create, :show]
   # why can't we include show in line 3
 end
